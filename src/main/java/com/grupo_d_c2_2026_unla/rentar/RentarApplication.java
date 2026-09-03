31990a197d38d92975e9b1f6232b6705fccb4c8f
Alan <alanfernandez2701@gmail.com>
2026-09-02 22:21:01 -0300

    primer commit

---
+++ src/main/java/com/grupo_d_c2_2026_unla/rentar/RentarApplication.java
@@ -0,0 +1,13 @@
+package com.grupo_d_c2_2026_unla.rentar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo_d_c2_2026_unla/rentar/RentarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo_d_c2_2026_unla.rentar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

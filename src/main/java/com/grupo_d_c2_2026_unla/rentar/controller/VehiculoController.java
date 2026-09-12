8d0ac1213767079380cf99a12bf1ea284d908858
Alan <alanfernandez2701@gmail.com>
2026-09-12 16:40:44 -0300

    se agrego baja logica, modificar, consulta

--- src/main/java/com/grupo_d_c2_2026_unla/rentar/controller/VehiculoController.java
+++ src/main/java/com/grupo_d_c2_2026_unla/rentar/controller/VehiculoController.java
@@ -6,10 +6,7 @@ import com.grupo_d_c2_2026_unla.rentar.dto.VehiculoResponseDTO;
 import com.grupo_d_c2_2026_unla.rentar.service.VehiculoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/vehiculos")
@@ -25,10 +22,27 @@ public class VehiculoController {
     // ALTA
     @PostMapping
     public ResponseEntity<VehiculoResponseDTO> crear(@RequestBody VehiculoRequestDTO vehiculo) {
-        // Reglas de negocio: patente única, estado inicial DISPONIBLE, activo=true
-        // (esa lógica va en el Service, el controller solo orquesta)
         VehiculoResponseDTO creado = vehiculoService.crear(vehiculo);
         return ResponseEntity.status(HttpStatus.CREATED).body(creado);
     }
+    // BAJA LÓGICA
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> baja(@PathVariable Integer id) {
+        vehiculoService.bajaLogica(id);
+        return ResponseEntity.noContent().build();
+    }
 
+    // MODIFICACIÓN
+    @PutMapping("/{id}")
+    public ResponseEntity<VehiculoResponseDTO> modificar(@PathVariable Integer id, @RequestBody VehiculoRequestDTO vehiculo) {
+        VehiculoResponseDTO modificado = vehiculoService.modificar(id, vehiculo);
+        return ResponseEntity.ok(modificado);
+    }
+
+    // OBTENER POR ID
+    @GetMapping("/{id}")
+    public ResponseEntity<VehiculoResponseDTO> buscarPorId(@PathVariable Integer id) {
+        VehiculoResponseDTO encontrado = vehiculoService.buscarPorId(id);
+        return ResponseEntity.ok(encontrado);
+    }
 }
